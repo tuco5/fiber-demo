edb53138d77b696b3274082b8c81a8cfbf035c02
tuco5 <tudor.comparan@gmail.com>
2024-12-19 23:22:50 -0600

    router

---
+++ router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/tuco5/fiber-demo/controller"
+)
+
+func NewNoteRouter(noteController *controller.NoteController) *fiber.App {
+	router := fiber.New()
+
+	router.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"status":  "success",
+			"message": "welcome to fiber demo",
+		})
+	})
+
+	router.Route("/note", func(router fiber.Router) {
+		router.Post("", noteController.Create)
+		router.Get("", noteController.FindAll)
+	})
+
+	router.Route("/note/:id", func(router fiber.Router) {
+		router.Get("", noteController.FindById)
+		router.Put("", noteController.Update)
+		router.Delete("", noteController.Delete)
+	})
+
+	return router
+}
