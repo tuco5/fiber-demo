95ae3dfcc9d754c3514ff4d71d88ea8fb3733801
tuco5 <tudor.comparan@gmail.com>
2024-12-19 19:29:50 -0600

    repository definition

---
+++ repository/note_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "github.com/tuco5/fiber-demo/model"
+
+type NoteRepository interface {
+	Save(note model.Note)
+	Update(note model.Note)
+	Delete(noteId int)
+	FindById(id int) (model.Note, error)
+	FindAll() []model.Note
+}
