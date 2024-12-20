5bd5c19e1827ccdee2f29f944c1b34619332c0af
tuco5 <tudor.comparan@gmail.com>
2024-12-19 19:29:33 -0600

    connect server to DB

--- server.go
+++ server.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/tuco5/fiber-demo/config"
 )
 
 func main() {
-	app := fiber.New()
+	fmt.Println("Starting server...")
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	env, err := config.LoadEnv(".")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.ConnectDB(&env)
+
+	app := fiber.New()
+	api := app.Group("/api")
+
+	router_v1 := fiber.New()
+	api.Mount("/v1", router_v1)
+
+	router_v1.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
 
