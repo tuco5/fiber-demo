004d948fcc12c59ee4b9d2b742066193bee8bf2e
tuco5 <tudor.comparan@gmail.com>
2024-12-19 18:31:55 -0600

    config load env

--- config/load_env.go
+++ config/load_env.go
@@ -1,5 +1,24 @@
 package config
 
+import "github.com/spf13/viper"
+
 type Config struct {
 	DBUrl string `mapstructure:"DB_URL"`
+}
+
+func LoadEnv(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
