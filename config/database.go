c023f6e783ff7a18029cb49ea3e09ef1d35a41b7
tuco5 <tudor.comparan@gmail.com>
2024-12-19 19:05:50 -0600

    use helper error

--- config/database.go
+++ config/database.go
@@ -3,16 +3,14 @@ package config
 import (
 	"fmt"
 
+	"github.com/tuco5/fiber-demo/helper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB(config *Config) *gorm.DB {
 	db, err := gorm.Open(postgres.Open(config.DBUrl), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
-	}
+	helper.ErrorPanic(err)
 
 	fmt.Println("Connected to DB")
 	return db
